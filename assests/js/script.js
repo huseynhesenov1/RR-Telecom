a6772cab919668f350253624ed3afbb360f9c00f
huseyn-dev <huseyn.developer.15@gmail.com>
2025-07-10 17:05:23 +0400

    email sender

---
+++ assests/js/script.js
@@ -0,0 +1,26 @@
+// const formInput = document.querySelectorAll(".form-control");
+//     (function() {
+//     emailjs.init({
+//       publicKey: "EW_BngZpDF48NcSYA",
+//     });
+// })();
+// window.onload = function() {
+//     document.getElementById('contact-form').addEventListener('submit', function(event) {
+       
+//             if (!formInput[0].value || !formInput[1].value || !formInput[2].value || !formInput[3].value || !formInput[4].value) {
+//                 alert("Please, Fill Input");
+//             }else{
+//             event.preventDefault();
+//             emailjs.sendForm('service_6rbyrzt', 'template_2rh3b2i', this)
+//                 .then(() => {
+//                     console.log('SUCCESS!');
+//                     alert("Succesful")
+//                 }, (error) => {
+//                     console.log('FAILED...', error);
+//                 });
+//             }
+//         // }
+//     });
+// }
+
+
